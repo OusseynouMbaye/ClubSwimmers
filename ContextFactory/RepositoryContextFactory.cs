c224365665992d89d08dc6254a79ed1209ea547d
Ouzinem <ouzinem@hotmail.com>
2023-07-12 09:05:56 -0400

    creating db context for migration

---
+++ ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
+
+namespace ClubSwimmers.ContextFactory
+{
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+    {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsetting.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<RepositoryContext>()
+                .UseSqlite(configuration.GetConnectionString("ClubswimmerString"),
+                b => b.MigrationsAssembly("ClubSwimmers"));
+
+            return new RepositoryContext(builder.Options);
+        }
+    }
+}
